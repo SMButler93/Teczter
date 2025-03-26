fb9e668efbb4ef1c1da606c0f9a2c0780d4efcc0
Scott Butler <sbutler@czarnikow.com>
2025-03-26 22:06:44 +0000

    create providers for tests.

---
+++ Teczter.Domain.Tests/TestStepEntityTests/TestStepInstanceProvider.cs
@@ -0,0 +1,33 @@
+﻿using Teczter.Domain.Entities;
+
+namespace Teczter.Domain.Tests.TestStepEntityTests;
+
+public static class TestStepInstanceProvider
+{
+    public static List<TestStepEntity> GetMultipleBasicTestStepInstances(int numberOfInstances)
+    {
+        var steps = new List<TestStepEntity>();
+
+        for (var i = 1; i <= numberOfInstances; i++)
+        {
+            var step = new TestStepEntity
+            {
+                StepPlacement = i,
+                Instructions = "Step" + i.ToString()
+            };
+
+            steps.Add(step);
+        }
+
+        return steps;
+    }
+
+    public static TestStepEntity GetBasicTestStepInstance()
+    {
+        return new TestStepEntity
+        {
+            StepPlacement = 1,
+            Instructions = "Step 1"
+        };
+    }
+}
