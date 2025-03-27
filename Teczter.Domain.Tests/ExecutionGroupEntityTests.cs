f884c5f34a5c440ae4ea6832914dec1e40b22e11
Scott Butler <sbutler@czarnikow.com>
2025-03-27 19:17:30 +0000

    Refactor domain tests and add new tests to Test service.

---
+++ Teczter.Domain.Tests/ExecutionGroupEntityTests.cs
@@ -0,0 +1,122 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using Teczter.Domain.Entities;
+using Teczter.Domain.Enums;
+
+namespace Teczter.Domain.Tests;
+
+[TestFixture]
+public class ExecutionGroupEntityTests
+{
+    private ExecutionGroupEntity _sut = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new ExecutionGroupEntity
+        {
+            CreatedById = 1,
+            RevisedById = 1,
+            IsDeleted = false,
+            ExecutionGroupName = "Basic Instance",
+            SoftwareVersionNumber = "1.1.1",
+            Executions = GetMultipleBasicExecutionInstances(2)
+        };
+    }
+
+    [Test]
+    public void DeleteExecutionGroup_WhenDeleted_ShouldDeleteEveryOwnedExecution()
+    {
+        //Act:
+        _sut.Delete();
+
+        //Assert:
+        _sut.IsDeleted.ShouldBeTrue();
+        _sut.Executions.ShouldAllBe(x => x.IsDeleted);
+    }
+
+    [Test]
+    public void AddExecution_WhenAdded_ShouldBePresentInCollectionOfExecutions()
+    {
+        //Arrange:
+        var newExecution = new ExecutionEntity
+        {
+            CreatedById = 1,
+            RevisedById = 1,
+            IsDeleted = false,
+            TestId = 999
+        };
+
+        //Act:
+        _sut.AddExecution(newExecution);
+
+        //Assert:
+        _sut.Executions.Count.ShouldBe(3);
+        _sut.Executions.ShouldContain(newExecution);
+    }
+
+    [Test]
+    public void AddNote_WhenAdded_ShouldBePresentInCollectionOfNotes()
+    {
+        //Arrange
+        var note = "new note";
+
+        //Act:
+        _sut.AddNote(note);
+
+        //Assert:
+        _sut.ExecutionGroupNotes.Count.ShouldBe(1);
+        _sut.ExecutionGroupNotes.ShouldContain(note);
+
+    }
+
+    [Test]
+    public void CloneExecutionGroup_WhenCloned_ShouldHaveSameValuesExceptForNameAndAuditableProperties()
+    {
+        //Arrange:
+        _sut.Executions[0].Test = GetBasicTestInstance();
+
+        //Act:
+        var clonedGroup = _sut.CloneExecutionGroup("new execution group", "2.2.2");
+
+        //Assert:
+        clonedGroup.Executions.Count.ShouldBe(_sut.Executions.Count);
+        clonedGroup.Executions[0].CreatedOn.ShouldNotBe(_sut.Executions[0].CreatedOn);
+        clonedGroup.ExecutionGroupName.ShouldNotBe(_sut.ExecutionGroupName);
+        clonedGroup.CreatedOn.ShouldNotBe(_sut.CreatedOn);
+        clonedGroup.Executions[0].Test.Title.ShouldBe(_sut.Executions[0].Test.Title);
+        clonedGroup.Executions[0].Test.TestSteps.Count.ShouldBe(_sut.Executions[0].Test.TestSteps.Count);
+    }
+
+    private static TestEntity GetBasicTestInstance()
+    {
+        return new TestEntity()
+        {
+            IsDeleted = false,
+            Title = "Basic test instance.",
+            Description = "Basic instance for testing.",
+            OwningDepartment = Department.Operations,
+            TestSteps = []
+        };
+    }
+
+    private static List<ExecutionEntity> GetMultipleBasicExecutionInstances(int numberOfInstances)
+    {
+        var executions = new List<ExecutionEntity>();
+
+        for (var i = 1; i <= numberOfInstances; i++)
+        {
+            var execution = new ExecutionEntity
+            {
+                CreatedById = 1,
+                RevisedById = 1,
+                IsDeleted = false,
+                TestId = i,
+            };
+
+            executions.Add(execution);
+        }
+
+        return executions;
+    }
+}
---
+++ Teczter.Domain.Tests/TestStepEntityTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using Teczter.Domain.Entities;
+using Teczter.Domain.Exceptions;
+
+namespace Teczter.Domain.Tests;
+
+[TestFixture]
+public class TestStepEntityTests
+{
+    private TestStepEntity _sut = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new TestStepEntity
+        {
+            StepPlacement = 1,
+            Instructions = "Step 1"
+        };
+    }
+
+    [Test]
+    public void AddLinkUrl_WhenNotValid_ShouldThrow()
+    {
+        //Arrange:
+        var invalidUrl = "invalidUrl.www";
+
+        //Act & Assert:
+        Should.Throw<TeczterValidationException>(() => _sut.AddLinkUrl(invalidUrl));
+    }
+
+    [Test]
+    public void AddLinkUrl_WhenValid_ShouldNotThrow()
+    {
+        //Arrange:
+        var validUrl = "www.validUrl.com";
+
+        //Act & Assert:
+        Should.NotThrow(() => _sut.AddLinkUrl(validUrl));
+    }
+
+    [Test]
+    public void RemoveLinkUrl_WhenRemoved_ShouldNotBePresentInCollection()
+    {
+        //Arrange:
+        var url = "www.Url.com";
+        var currentRevisedOn = _sut.RevisedOn;
+        _sut.AddLinkUrl(url);
+
+        //Act:
+        _sut.RemoveLinkUrl(url);
+
+        //Assert:
+        _sut.Urls.ShouldNotContain(url);
+        _sut.RevisedOn.ShouldNotBe(currentRevisedOn);
+    }
+
+    [Test]
+    public void Delete_WhenDeleted_ShouldBeUpdatedToReflectDeletion()
+    {
+        //Arrange:
+        var currentrevisedOn = _sut.RevisedOn;
+
+        //Act:
+        _sut.Delete();
+
+        //Assert:
+        _sut.IsDeleted.ShouldBeTrue();
+        _sut.RevisedOn.ShouldNotBe(currentrevisedOn);
+    }
+
+    [Test]
+    public void Update_WhenUpdated_ShouldReflectNewValues()
+    {
+        //Arrange:
+        var newStepPlacement = 5;
+        var newInstructions = "new instructions.";
+        var url = "www.url.com";
+
+        //Act:
+        _sut.Update(newStepPlacement, newInstructions, [url]);
+
+        //Assert:
+        _sut.StepPlacement.ShouldBe(newStepPlacement);
+        _sut.Instructions.ShouldBe(newInstructions);
+        _sut.Urls.Count.ShouldBe(1);
+        _sut.Urls.ShouldContain(url);
+    }
+}
