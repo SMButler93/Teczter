fec2afe3ef2ba228659a7e383f9ba96b2db5c6b9
Scott Butler <sbutler@czarnikow.com>
2025-04-19 14:48:34 +0100

    Move backend code into it's own folder and create frontend folder with initial files.

---
+++ Backend/Teczter.Services/ExecutionService.cs
@@ -0,0 +1,20 @@
+﻿using Teczter.Domain.Entities;
+using Teczter.Services.AdapterInterfaces;
+using Teczter.Services.ServiceInterfaces;
+
+namespace Teczter.Services;
+
+public class ExecutionService : IExecutionService
+{
+    private readonly IExecutionAdapter _executionAdapter;
+
+    public ExecutionService(IExecutionAdapter executionAdapter)
+    {
+        _executionAdapter = executionAdapter;
+    }
+
+    public async Task<ExecutionEntity?> GetExecutionById(int id)
+    {
+        return await _executionAdapter.GetExecutionById(id);
+    }
+}
