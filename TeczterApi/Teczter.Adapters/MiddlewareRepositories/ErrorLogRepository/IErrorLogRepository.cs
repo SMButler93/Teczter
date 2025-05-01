b87f6af73e4a358e4fabff2d93170dc629694eac
Scott Butler <sbutler@czarnikow.com>
2025-05-01 22:33:49 +0100

    Error logging Middleware

---
+++ TeczterApi/Teczter.Adapters/MiddlewareRepositories/ErrorLogRepository/IErrorLogRepository.cs
@@ -0,0 +1,8 @@
+﻿using Teczter.Data.MiddlewareModels;
+
+namespace Teczter.Adapters.MiddlewareRepositories.ErrorLogRepository;
+
+public interface IErrorLogRepository
+{
+    public Task LogError(ErrorLog log);
+}
