b94b1dc50a84183f9bfe0b1435bf8977a81d744d
Scott Butler <ScottMButler93@gmail.com>
2026-02-18 23:06:28 +0000

    ..

--- TeczterApi/Teczter.Infrastructure/Auth/DesignTimeUserDbContextFactory.cs
+++ TeczterApi/Teczter.Infrastructure/Auth/DesignTimeUserDbContextFactory.cs
@@ -1,10 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Options;
 
 namespace Teczter.Infrastructure.Auth;
 
-public class DesignTimeUserDbContextFactory() : IDesignTimeDbContextFactory<UserDbContext>
+public class DesignTimeUserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
 {
     public UserDbContext CreateDbContext(string[] args)
     {
